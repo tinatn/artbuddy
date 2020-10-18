aaeccf2d9b7dd9298f8da011339d9d5598176c4b
brandonyan5 <brandonyan5@gmail.com>
2020-10-18 11:33:28 -0400

    try 18

---
+++ src/components/Icons.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="icons">
+    <div v-if="Math.random() > 0.5">
+    <svg width="30px" height="30px" viewBox="0 0 16 16" class="bi bi-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+</svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Icons',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
